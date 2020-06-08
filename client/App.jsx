e7acaad5046e1cdeaa89ab391e1f4d66f7e637b1
Adam Tiller <a.j.tiller@gmail.com>
2020-06-08 16:12:10 -0700

    State and splash blocking.

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const App = () => (
-  <div>Hello world!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      story: [],
+      root: 0,
+      eNode: 0,
+      sNode: 0,
+      visited: false,
+      amEditing: true,
+    };
+
+    this.toggleVisited = this.toggleVisited.bind(this);
+  }
+
+  toggleVisited() {
+    const { visited } = this.state;
+    this.setState({
+      visited: !visited,
+    });
+  }
+
+  render() {
+    const { visited } = this.state;
+    return (
+      visited ? (
+        <div>Not Splash</div>
+      ) : (
+        <div>
+          Splash
+          <button onClick={this.toggleVisited}>Play</button>
+        </div>
+      )
+    );
+  }
+}
+
 
 export default App;
